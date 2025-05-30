421fe621c8681f384be9d85982767028323e6cb6
sahil0126 <sahilrajurkar88@gmail.com>
2025-05-30 12:42:53 +0530

    added Exception

---
+++ Exceptions_in_CS/Exceptions_in_CS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            try
+            {
+
+                int[] num = { 1, 2, 3 };
+                Console.WriteLine(num[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //Console.WriteLine("Enter valid Index");
+            }
+            finally
+            {
+                Console.WriteLine("This is Finally Block");
+            }
+            */
+            CheckAge(26);
+
+        }
+        static void CheckAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new Exception("You are not Eligible");
+            }
+            else
+            {
+                Console.WriteLine("You are Eligible");
+            }
+        }
+    }
+}
