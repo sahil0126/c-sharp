2799946f481aac1255b51cb50998cc9425713242
sahil0126 <sahilrajurkar88@gmail.com>
2025-05-30 12:08:16 +0530

    completed interface

---
+++ OOP_5/OOP_5/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace OOP_5
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program Started");
+            Demo demo = new Demo();
+            demo.myFirstMethod();
+            demo.mySecondMethod();
+            Console.ReadLine();
+
+        }
+
+        interface IFirstInterface
+        {
+            void myFirstMethod();
+
+        }
+        interface ISecondInterface
+        {
+            void mySecondMethod();
+
+        }
+
+        class Demo : IFirstInterface, ISecondInterface
+        {
+            public void myFirstMethod()
+            {
+                Console.WriteLine("This is First Method Implementation");
+            }
+
+            public void mySecondMethod()
+            {
+                Console.WriteLine("This is Second Method Implementation");
+            }
+        }
+    }
+
+}
--- OOP_5/OOP_5/Program.cs
+++ OOP_5/OOP_5/Program.cs
@@ -4,34 +4,53 @@ namespace OOP_5
 {
     class Program
     {
-        public static void Main(String[] args)
-        {
+        //public static void Main(String[] args)
+        //{
             /*
             Console.WriteLine("Hello! ");
             AnimalImplementationClass aic = new AnimalImplementationClass();
             aic.animalSleep();
             aic.sleep();
              */
+            //Console.WriteLine("Program started");
 
             Tiger tiger = new Tiger();
-            tiger.AnimalSound();
-            tiger.Run();
-            Console.ReadLine();
-        }
+
+            //IAnimal tiger = new Tiger(); -- This will give default implementation
+            //tiger.AnimalSound();
+            //tiger.Run();
+            //Console.ReadLine();
+        //}
 
     }
 
     // using interface
 
+
+
+
     /*
      It is considered good practice to start with the letter "I" at the beginning of an interface, as it makes it easier for yourself and others to remember that it is an interface and not a class.
      */
 
+
+
+    /*
+     Notes on Interfaces:
+Like abstract classes, interfaces cannot be used to create objects (in the example above, it is not possible to create an "IAnimal" object in the Program class)
+Interface methods do not have a body - the body is provided by the "implement" class
+On implementation of an interface, you must override all of its methods
+Interfaces can contain properties and methods, but not fields/variables
+Interface members are by default abstract and public
+An interface cannot contain a constructor (as it cannot be used to create objects)
+    */
+
     interface IAnimal
     {
-        void AnimalSound()
+        void AnimalSound() // This is using a default implementation (valid in C# 8+)
         {
-            Console.WriteLine("Hello ");
+            Console.WriteLine("This is a default implementation");
+
         }
         void Run();
     }
