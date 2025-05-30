db58c7025ddc1d82bae34b0fee0056a487d4a5ab
sahil0126 <sahilrajurkar88@gmail.com>
2025-05-30 12:16:48 +0530

    added File Program

---
+++ File/File/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+
+string writeText = "This is a File";
+File.WriteAllText("csharpfile.txt", writeText);
+
+string readText = File.ReadAllText("csharpfile.txt");
+Console.WriteLine(readText);
